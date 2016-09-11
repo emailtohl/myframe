11f40b91b7b9e78a01ecbff00dc0bbb86e879282
helei <emailtohl@163.com>
2016-09-11 17:01:39 +0800

    创建分支，一个管理对象实例的子项目，模仿Spring容器

---
+++ frame-context/src/main/java/com/github/emailtohl/frame/context/Bean.java
@@ -0,0 +1,60 @@
+package com.github.emailtohl.frame.context;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+/**
+ * 对容器进行建模，包含依赖注入的必要信息
+ * @author HeLei
+ */
+public class Bean implements Serializable, Comparable<Object> {
+	private static final long serialVersionUID = 6030804224172961375L;
+	/**
+	 * 对象实例
+	 */
+	private Object instance;
+	/**
+	 * 对象的类型
+	 */
+	private Class<?> type;
+	/**
+	 * 依赖列表
+	 */
+	private Set<Bean> dependencies = new HashSet<Bean>();
+	/**
+	 * Bean的名字，默认为其类名，小写首字母
+	 */
+	private String name;
+
+	@Override
+	public int compareTo(Object o) {
+		return 0;
+	}
+	
+	public Object getInstance() {
+		return instance;
+	}
+	public void setInstance(Object instance) {
+		this.instance = instance;
+	}
+	public Class<?> getType() {
+		return type;
+	}
+	public void setType(Class<?> type) {
+		this.type = type;
+	}
+	public Set<Bean> getDependencies() {
+		return dependencies;
+	}
+	public void setDependencies(Set<Bean> dependencies) {
+		this.dependencies = dependencies;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ frame-context/src/main/java/com/github/emailtohl/frame/context/Container.java
@@ -0,0 +1,85 @@
+package com.github.emailtohl.frame.context;
+
+import java.lang.reflect.Field;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+import javax.inject.Inject;
+
+import com.github.emailtohl.frame.util.BeanTools;
+import com.github.emailtohl.frame.util.PackageScanner;
+/**
+ * 作为容器，存储依赖关系的Bean对象，它继承TreeMap，key是Bean对象的类型，value是Bean对象的实例
+ * @author HeLei
+ */
+public class Container extends TreeMap<Class<?>, Object> {
+	private static final long serialVersionUID = -5858462858295708970L;
+	private static final Logger logger = LogManager.getLogManager().getLogger(BeanTools.class.getName());
+	
+	private Set<Bean> beanSet = new HashSet<Bean>();
+	
+	public Container(String packagePath) {
+		super();
+		Set<Class<?>> classSet = PackageScanner.getClasses(packagePath);
+		filter(classSet);
+		fetch(classSet);
+	}
+	
+	/**
+	 * 过滤无关的Class
+	 */
+	private void filter(Set<Class<?>> classSet) {
+		Iterator<Class<?>> i = classSet.iterator();
+		while (i.hasNext()) {
+			Component c = i.next().getAnnotation(Component.class);
+			if (c == null) {
+				i.remove();
+			}
+		}
+	}
+	
+	/**
+	 * 将类的信息填充进Bean对象中，进行建模工作
+	 */
+	private void fetch(Set<Class<?>> classSet) {
+		for (Class<?> clz : classSet) {
+			Bean b = new Bean();
+			Component c = clz.getAnnotation(Component.class);
+			String name = c.name();
+			if (name.isEmpty()) {
+				name = clz.getSimpleName();
+				name = name.substring(0, 1).toLowerCase() + name.substring(1);
+			}
+			b.setName(name);
+			b.setType(clz);
+			Object instance;
+			try {
+				instance = clz.newInstance();
+			} catch (InstantiationException | IllegalAccessException e) {
+				logger.log(Level.SEVERE, clz.getName() + "不能实例化，必须提供一个无参构造器", e);
+				e.printStackTrace();
+				throw new IllegalArgumentException(clz.getName() + "不能实例化，必须提供一个无参构造器", e);
+			}
+			b.setInstance(instance);
+			// 处理依赖关系，若Bean不存在会递归分析
+			for (Entry<String, Field> e : BeanTools.getFieldMap(instance).entrySet()) {
+				Field f = e.getValue();
+				f.setAccessible(true);
+				Inject inj = f.getAnnotation(Inject.class);
+				if (inj == null) {
+					continue;
+				}
+				b.getDependencies().add(new Bean());
+			}
+			
+			beanSet.add(b);
+		}
+	}
+}
