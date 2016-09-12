76b5d04c5dd0562e8e5d436b4dd511cbf3afa0d7
helei <emailtohl@163.com>
2016-09-12 09:47:12 +0800

    TODO

--- frame-ioc/src/main/java/com/github/emailtohl/frame/context/Bean.java
+++ frame-ioc/src/main/java/com/github/emailtohl/frame/context/Bean.java
@@ -1,7 +1,9 @@
 package com.github.emailtohl.frame.context;
 
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 /**
  * 对容器进行建模，包含依赖注入的必要信息
@@ -10,9 +12,9 @@ import java.util.Set;
 public class Bean implements Serializable, Comparable<Object> {
 	private static final long serialVersionUID = 6030804224172961375L;
 	/**
-	 * 对象实例
+	 * 对象实例map，一个类型下有多个实例，他们以实例名为key
 	 */
-	private Object instance;
+	private Map<String, Object> instances = new HashMap<String, Object>();
 	/**
 	 * 对象的类型
 	 */
@@ -21,39 +23,35 @@ public class Bean implements Serializable, Comparable<Object> {
 	 * 依赖列表
 	 */
 	private Set<Bean> dependencies = new HashSet<Bean>();
-	/**
-	 * Bean的名字，默认为其类名，小写首字母
-	 */
-	private String name;
 
 	@Override
 	public int compareTo(Object o) {
 		return 0;
 	}
+
 	
-	public Object getInstance() {
-		return instance;
+	public Map<String, Object> getInstances() {
+		return instances;
 	}
-	public void setInstance(Object instance) {
-		this.instance = instance;
+
+	public void setInstances(Map<String, Object> instances) {
+		this.instances = instances;
 	}
+
 	public Class<?> getType() {
 		return type;
 	}
+
 	public void setType(Class<?> type) {
 		this.type = type;
 	}
+
 	public Set<Bean> getDependencies() {
 		return dependencies;
 	}
+
 	public void setDependencies(Set<Bean> dependencies) {
 		this.dependencies = dependencies;
 	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	
+
 }
--- frame-ioc/src/main/java/com/github/emailtohl/frame/context/Container.java
+++ frame-ioc/src/main/java/com/github/emailtohl/frame/context/Container.java
@@ -1,5 +1,10 @@
 package com.github.emailtohl.frame.context;
 
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.io.Serializable;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -18,18 +23,21 @@ import com.github.emailtohl.frame.util.PackageScanner;
  * 作为容器，存储依赖关系的Bean对象，它继承TreeMap，key是Bean对象的类型，value是Bean对象的实例
  * @author HeLei
  */
-public class Container extends TreeMap<Class<?>, Object> {
+public class Container implements Serializable {
 	private static final long serialVersionUID = -5858462858295708970L;
 	private static final Logger logger = LogManager.getLogManager().getLogger(BeanTools.class.getName());
-	
+	private TreeMap<Class<?>, Bean> context = new TreeMap<Class<?>, Bean>();
 	private Set<Bean> beanSet = new HashSet<Bean>();
 	
 	public Container(String packagePath) {
 		super();
 		Set<Class<?>> classSet = PackageScanner.getClasses(packagePath);
+		// 第一步，过滤无关的Class
 		filter(classSet);
+		// 第二步，对依赖关系进行建模
 		for (Class<?> clz : classSet) {
-			beanSet.add(analyze(clz));
+			Bean b = new Bean();
+			setDependencies(b, clz);
 		}
 	}
 	
@@ -47,7 +55,75 @@ public class Container extends TreeMap<Class<?>, Object> {
 	}
 	
 	/**
-	 * 分析类的信息，然后填充进Bean对象中，进行建模工作
+	 * 对依赖关系进行建模
+	 * @param classSet
+	 */
+	private void setDependencies(Bean b, Class<?> clz) {
+		// 首先，在构造器中查找依赖
+		Set<Bean> sb = new HashSet<Bean>();
+		for (Constructor<?> constructor : clz.getConstructors()) {
+			Component c = constructor.getAnnotation(Component.class);
+			if (c == null) {
+				continue;
+			}
+			for (Class<?> cl : constructor.getParameterTypes()) {
+				Bean be = context.get(cl);
+				if (be == null) {
+					be = new Bean();
+				}
+				sb.add(be);
+			}
+			b.getDependencies().addAll(sb);
+		}
+		
+		// 然后，在符合JavaBean规范的setter方法中查找依赖
+		sb.clear();
+		try {
+			for (PropertyDescriptor p : Introspector.getBeanInfo(clz, Object.class).getPropertyDescriptors()) {
+				Bean be = context.get(p.getPropertyType());
+				if (be == null) {
+					be = new Bean();
+				}
+				sb.add(be);
+			}
+		} catch (IntrospectionException e) {
+			e.printStackTrace();
+			logger.log(Level.SEVERE, "分析Bean的Setter方法发生异常，依赖注入可能失败", e);
+		}
+		b.getDependencies().addAll(sb);
+		
+		// 最后，在Field字段上查找依赖
+		sb.clear();
+		for (Entry<String, Field> e : BeanTools.getFieldMap(clz).entrySet()) {
+			Bean be = context.get(e.getValue());
+			if (be == null) {
+				be = new Bean();
+			}
+			sb.add(be);
+		}
+		b.getDependencies().addAll(sb);
+	}
+	
+	/**
+	 * 获取Bean的名字
+	 * @param clz
+	 * @return
+	 */
+	private String getBeanName(Class<?> clz) {
+		Component c = clz.getAnnotation(Component.class);
+		if (c == null) {
+			return null;
+		}
+		String name = c.name();
+		if (name.isEmpty()) {
+			name = clz.getSimpleName();
+			name = name.substring(0, 1).toLowerCase() + name.substring(1);
+		}
+		return name;
+	}
+	
+	/**
+	 * 第二步，分析类的依赖关系，进行建模
 	 */
 	private Bean analyze(Class<?> clz) {
 		Bean b = new Bean();
@@ -60,7 +136,6 @@ public class Container extends TreeMap<Class<?>, Object> {
 			name = clz.getSimpleName();
 			name = name.substring(0, 1).toLowerCase() + name.substring(1);
 		}
-		b.setName(name);
 		b.setType(clz);
 		Object instance;
 		try {
@@ -70,7 +145,6 @@ public class Container extends TreeMap<Class<?>, Object> {
 			e.printStackTrace();
 			throw new IllegalArgumentException(clz.getName() + "不能实例化，必须提供一个无参构造器", e);
 		}
-		b.setInstance(instance);
 		// 处理依赖关系
 		for (Entry<String, Field> e : BeanTools.getFieldMap(instance).entrySet()) {
 			Field f = e.getValue();
